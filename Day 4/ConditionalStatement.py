24fd4148f0bd7c22393d3e73f1e91e5711b1af40
sharang <sharangherlekar@gmail.com>
2021-09-14 20:58:55 +0530

    1

---
+++ Day 4/ConditionalStatement.py
@@ -0,0 +1,25 @@
+'''# Conditional Statement
+1. if
+2. if else
+3. if elif (For multiple options)
+4. nested if()
+
+if condition: Indentation is important
+    ------
+else:
+    ------
+
+elif condition:
+   ------
+   -----
+elif:
+   -----
+   -----
+else '''
+
+if 34>5:
+    print("Welcome")
+elif 45>6:
+    print("Hi")
+else:
+    print("oops")
